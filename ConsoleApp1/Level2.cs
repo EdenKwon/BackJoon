12a9563f5228da4aff35e44034111cc1ab0c415b
EdenKwon <EdenKwon@github.com>
2023-03-14 16:42:58 +0900

    # lv2_1 - 두 수 비교하기

---
+++ ConsoleApp1/Level2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackJoon
+{
+    internal class Level2
+    {
+        static void Main(string[] args)
+        {
+            ex1();
+        }
+
+        static void ex1()
+        {
+            String s = Console.ReadLine();
+            String[] ss = s.Split();
+
+            int a = int.Parse(ss[0]);
+            int b = int.Parse(ss[1]);
+
+            if (a > b)
+            {
+                Console.WriteLine(">");
+            }
+
+            else if (a < b)
+            {
+                Console.WriteLine("<");
+            }
+
+            else
+            {
+                Console.WriteLine("==");
+            }
+        }
+    } 
+}
