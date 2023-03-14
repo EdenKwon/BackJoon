250362f747d84feb09edcf58439176aecc1bb88d
EdenKwon <EdenKwon@github.com>
2023-03-14 12:30:01 +0900

    #2 a + b 메인 함수 정리

--- ConsoleApp1/1.helloworld.cs
+++ ConsoleApp1/1.helloworld.cs
@@ -1,13 +1,24 @@
-﻿/*
-namespace ConsoleApp1
+﻿namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            ex2();
+        }
+
+        static void ex1()
+        {
             Console.WriteLine("Hello World!");
         }
 
+        static void ex2()
+        {
+            String s = Console.ReadLine();
+            String[] ss = s.Split();
+            int a = int.Parse(ss[0]);
+            int b = int.Parse(ss[1]);
+            Console.WriteLine(a + b);
+        }
     }
 }
-*/
