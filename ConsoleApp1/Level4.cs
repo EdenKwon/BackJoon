5769c13ddef00bb40a5d72582f995a7f8a224780
EdenKwon <EdenKwon@github.com>
2023-03-15 17:57:56 +0900

    # lv4_1 - 입력값 개수 세기

---
+++ ConsoleApp1/Level4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackJoon
+{
+    internal class Level4
+    {
+
+        static int ParseToInt(string s) => int.Parse(s);
+        static void Print(int num) { Console.WriteLine(num); }
+
+        static void Main()
+        {
+            ex1();
+        }
+
+        static void ex1()
+        {
+            string s = Console.ReadLine();
+            int a = int.Parse(s);
+
+            string num = Console.ReadLine();
+            string[] ss = num.Split(" ");
+            string t = Console.ReadLine();
+
+            int result = Array.FindAll(ss, x => x == t).Length;
+            Print(result);
+        }
+        
+    }
+}
