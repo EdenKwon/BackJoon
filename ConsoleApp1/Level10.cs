6c696721098dfb7b764c8e8800abed89d4f2b110
EdenKwon <EdenKwon@github.com>
2023-03-20 19:26:27 +0900

    # lv10_1 시간복잡도 구하기 - lvl10 END

---
+++ ConsoleApp1/Level10.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackJoon
+{
+    internal class Level10
+    {
+        static int ToInt(string s) => int.Parse(s);
+        static void Print(int num) { Console.WriteLine(num); }
+        static void Print(int num1, int num2) { Console.WriteLine("{0} {1}", num1, num2); }
+        static void Print(long num) { Console.WriteLine(num); }
+        static void Print(double num) { Console.WriteLine(num); }
+        static void Print(string s) { Console.WriteLine(s); }
+        private static string Read() => Console.ReadLine();
+
+        static void Main(String[] args)
+        {
+            ex1();
+        }
+
+        static int MenOfPassion(int []A, int n)
+        {
+            int i = n / 2;
+            return A[i];
+        }
+
+        static void ex1()
+        {
+            string s = Read();
+            int n = ToInt(s);
+            int[] A = new int[n];
+
+            MenOfPassion(A, n);
+            Print(1, 0);
+        }
+    }
+}
