e8be798340102e36f40952aeb49bbaf8eb4cb396
EdenKwon <EdenKwon@github.com>
2023-03-20 16:41:41 +0900

    # lv9_3 직사각형을 만족시키는 마지막 좌표 구하기

--- ConsoleApp1/Level9.cs
+++ ConsoleApp1/Level9.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,9 +15,19 @@ namespace BackJoon
         static void Print(string s) { Console.WriteLine(s); }
         private static string Read() => Console.ReadLine();
 
+        static int Max(int a, int b, int c)
+        {
+            if (a >= b && a >= c)
+                return a;
+            else if (a <= b && c <= b)
+                return c;
+            else
+                return c;
+        }
+
         static void Main(String[] args)
         {
-            ex2();
+            ex3();
         }
 
         static int getMinLen(int a, int b) // a<=b
@@ -30,6 +41,36 @@ namespace BackJoon
         {
             if (a > b) return b;
             else return a;
+        }
+
+        struct Point
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+
+        static Point findPoint(Point p1, Point p2, Point p3)
+        {
+            if (p1.X == p2.X)
+            {
+                return new Point(p3.X, (p3.Y == p2.Y) ? p1.Y : p2.Y);
+            }
+
+            else if (p1.X == p3.X)
+            {
+                return new Point(p2.X, (p2.Y == p1.Y) ? p3.Y : p1.Y);
+            }
+
+            else
+            {
+                return new Point(p1.X, (p1.Y == p2.Y) ? p3.Y : p2.Y);
+            }
         }
 
         static void ex1()
@@ -53,6 +94,23 @@ namespace BackJoon
             int h = init[3];
 
             Print(compareMin(getMinLen(x, w), getMinLen(y, h)));
+        }
+
+        static Point ReadPoint()
+        {
+            string s = Read();
+            int[] p = s.Split().Select(x => ToInt(x)).ToArray();
+            return new Point(p[0], p[1]);
+        }
+        static void ex3()
+        {
+            var Point1 = ReadPoint();
+            var Point2 = ReadPoint();
+            var Point3 = ReadPoint();
+            var Point4 = new Point();
+
+            Point4 = findPoint(Point1, Point2, Point3);
+            Print(Point4.X, Point4.Y);
         }
     }
 }
