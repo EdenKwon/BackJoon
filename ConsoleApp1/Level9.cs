54e0a1f8e88054b95b7335afc55f702df26c9a49
EdenKwon <EdenKwon@github.com>
2023-03-20 14:46:57 +0900

    # lv9_1 - 직사격형 넓이 구하기

---
+++ ConsoleApp1/Level9.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackJoon
+{
+    internal class Level9
+    {
+        static int ToInt(string s) => int.Parse(s);
+        static void Print(int num) { Console.WriteLine(num); }
+        static void Print(int num1, int num2) { Console.WriteLine("{0} {1}", num1, num2); }
+        static void Print(string s) { Console.WriteLine(s); }
+        private static string Read() => Console.ReadLine();
+
+        static void Main(String[] args)
+        {
+            ex1();
+        }
+
+        static void ex1()
+        {
+            string s = Read();
+            int a = ToInt(s);
+
+            string ss = Read();
+            int b = ToInt(ss);
+
+            Print(a * b);
+        }
+    }
+}
