3f95dde5311b65802d64358befa5af3d5407915e
EdenKwon <EdenKwon@github.com>
2023-03-20 14:48:06 +0900

    # lv9_2 직사각형에서의 탈출

--- ConsoleApp1/Level9.cs
+++ ConsoleApp1/Level9.cs
@@ -16,7 +16,20 @@ namespace BackJoon
 
         static void Main(String[] args)
         {
-            ex1();
+            ex2();
+        }
+
+        static int getMinLen(int a, int b) // a<=b
+        {
+            if ((double)b / 2 <= a)
+                return b - a;
+            else return a;
+        }
+
+        static int compareMin(int a, int b)
+        {
+            if (a > b) return b;
+            else return a;
         }
 
         static void ex1()
@@ -28,6 +41,18 @@ namespace BackJoon
             int b = ToInt(ss);
 
             Print(a * b);
+        }
+
+        static void ex2()
+        {
+            string s = Read();
+            int[] init = s.Split().Select(x => ToInt(x)).ToArray();
+            int x = init[0];
+            int y = init[1];
+            int w = init[2];
+            int h = init[3];
+
+            Print(compareMin(getMinLen(x, w), getMinLen(y, h)));
         }
     }
 }
