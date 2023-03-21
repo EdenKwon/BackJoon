373da8e4cebcf7c699315c02904b7d5c0c8b6337
EdenKwon <EdenKwon@github.com>
2023-03-21 19:54:35 +0900

    # lv12_3 커트라인

--- ConsoleApp1/Level12.cs
+++ ConsoleApp1/Level12.cs
@@ -19,7 +19,7 @@ namespace BackJoon
 
         static void Main(String[] args)
         {
-            ex3();
+            ex6();
         }
 
         static void insertion_sort(int[] elem)
@@ -37,13 +37,49 @@ namespace BackJoon
             }
         }
 
+        static int find_middle(int[] arr, int num)
+        {
+            Array.Sort(arr);
+            return arr[num / 2];
+        }
+
+        static int find_average(int[] arr)
+        {
+            int sum = Enumerable.Sum(arr) / arr.Length;
+            return sum;
+        }
+
+        static int find_mode(int[] arr) //chatGPT
+        {
+            int[] modes = arr.GroupBy(x => x)
+                          .OrderBy(g => g.Count())
+                          .Select(g => g.Key)
+                          .ToArray();
+
+            if(modes.Length == 1)
+            {
+                return modes[0];
+            }
+            
+            else
+            {
+                return modes.OrderBy(m => m).Skip(1).First();
+            }
+        }
+
+        static int find_range(int[] arr)
+        {
+            Array.Sort(arr);
+            return arr[arr.Length-1]- arr[0];
+        }
+
         static void ex1()
         {
             string s = Read();
             int num = ToInt(s);
             int[] elem = new int[num];
 
-            for(int i = 0; i < num; i++)
+            for (int i = 0; i < num; i++)
             {
                 string element = Read();
                 elem[i] = ToInt(element);
@@ -52,18 +88,6 @@ namespace BackJoon
             insertion_sort(elem);
             //Array.Sort(elem);
             elem.ToList().ForEach(x => Print(x));
-        }
-
-        private static int find_middle(int[] arr)
-        {
-            Array.Sort(arr);
-            return arr[2];
-        }
-
-        private static int find_average(int[] arr)
-        {
-            int sum = Enumerable.Sum(arr) / arr.Length;
-            return sum;
         }
 
         static void ex2()
@@ -77,7 +101,6 @@ namespace BackJoon
             }
 
             Semaphore sem = new Semaphore(0, 1);
-
             Thread avgThread = new Thread(() => 
             {
                 Print(find_average(elem));
@@ -87,7 +110,7 @@ namespace BackJoon
             Thread midThread = new Thread(() => 
             {
                  sem.WaitOne();
-                 Print(find_middle(elem));
+                 Print(find_middle(elem, 5));
             });
 
             avgThread.Start();
@@ -110,7 +133,127 @@ namespace BackJoon
 
             Array.Sort(score);
             Array.Reverse(score);
-            Print(score[cut-1]);
+            Print(score[cut - 1]);
         }
+
+        public static void MergeSort(int[] arr)
+        {
+            if (arr.Length < 2)
+            {
+                return;
+            }
+
+            int mid = arr.Length / 2;
+            int[] left = new int[mid];
+            int[] right = new int[arr.Length - mid];
+
+            for (int i = 0; i < mid; i++)
+            {
+                left[i] = arr[i];
+            }
+
+            for (int i = mid; i < arr.Length; i++)
+            {
+                right[i - mid] = arr[i];
+            }
+
+            MergeSort(left);
+            MergeSort(right);
+
+            Merge(arr, left, right);
+        }
+
+        private static void Merge(int[] arr, int[] left, int[] right)
+        {
+            int i = 0, j = 0, k = 0;
+
+            while (i < left.Length && j < right.Length)
+            {
+                if (left[i] <= right[j])
+                {
+                    arr[k++] = left[i++];
+                }
+                else
+                {
+                    arr[k++] = right[j++];
+                }
+            }
+
+            while (i < left.Length)
+            {
+                arr[k++] = left[i++];
+            }
+
+            while (j < right.Length)
+            {
+                arr[k++] = right[j++];
+            }
+        }
+
+        static void ex4()
+        {
+            string s = Read();
+            int num = ToInt(s);
+            int[] elem = new int[num];
+
+            for (int i = 0; i < num; i++)
+            {
+                string element = Read();
+                elem[i] = ToInt(element);
+            }
+
+            MergeSort(elem);
+            elem.ToList().ForEach(x => Print(x));
+        }
+
+        static void ex6()
+        {
+            string s = Read();
+            int num = ToInt(s);
+            int[] elem = new int[num];
+
+            for (int i = 0; i < num; i++)
+            {
+                string element = Read();
+                elem[i] = ToInt(element);
+            }
+
+            Semaphore sem = new Semaphore(0, 1);
+            Thread[] threads = new Thread[]
+            {
+                new Thread(() => {
+                    Print(find_average(elem));
+                    sem.Release();
+                }),
+                new Thread(() =>
+                {
+                    sem.WaitOne();
+                    Print(find_middle(elem, num));
+                    sem.Release();
+                }),
+                new Thread(() =>
+                {
+                    sem.WaitOne();
+                    Print(find_mode(elem));
+                    sem.Release();
+                }),
+                new Thread(() =>
+                {
+                    sem.WaitOne();
+                    Print(find_range(elem));
+                    sem.Release();
+                })
+            };
+
+            foreach (Thread t in threads)
+            {
+                t.Start();
+            }
+            
+            foreach (Thread t in threads)
+            {
+                t.Join();
+            }
+        }        
     }
 }
